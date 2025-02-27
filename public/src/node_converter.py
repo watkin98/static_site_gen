ffa725bd4b26eee015ea0e9442faa76a8bc8b5b0
watkin <wvatkin1@gmail.com>
2025-02-26 17:48:14 -0700

    completed converter and test cases

--- public/src/node_converter.py
+++ public/src/node_converter.py
@@ -6,14 +6,14 @@ def text_node_to_html_node(text_node):
         case TextType.TEXT:
             return LeafNode(value=text_node.text) 
         case TextType.BOLD:
-            return
+            return LeafNode(tag='b', value=text_node.text)
         case TextType.ITALIC:
-            return
+            return LeafNode(tag='i', value=text_node.text)
         case TextType.CODE:
-            return
+            return LeafNode(tag='code', value=text_node.text)
         case TextType.LINK:
-            return
+            return LeafNode(tag='a', value=text_node.text, props={"href" : f"{text_node.url}"})
         case TextType.IMAGE:
-            return
+            return LeafNode(tag='img', value="", props={"src" : f"{text_node.url}", "alt" : f"{text_node.text}"})
         case __:
-            return
+            raise ValueError("Must input a valid TextType")
--- public/tests/test_node_converter.py
+++ public/tests/test_node_converter.py
@@ -4,9 +4,45 @@ from src.textnode import *
 from src.node_converter import *
 
 class TestNodeConverter(unittest.TestCase):
-    def test(self):
+    def test_text_node(self):
         text_node = TextNode("yolo mcswaggins", TextType.TEXT)
         leafnode = text_node_to_html_node(text_node)
-        print(f"LeafNode: {leafnode.to_html()}")
-        print(f"leafnode: {type(leafnode)}")
-        self.assertIs(leafnode, LeafNode)
+        self.assertEqual(type(leafnode), LeafNode)
+
+    def test_text_provided(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold move cotton, let's see if it pays off", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, "Bold move cotton, let's see if it pays off")
+
+    def test_italic(self):
+        node = TextNode("it's a me, a mario", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'i')
+        self.assertEqual(html_node.value, "it's a me, a mario")
+
+    def test_code(self):
+        node = TextNode("dey terk er codr jerbs", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'code')
+        self.assertEqual(html_node.value, "dey terk er codr jerbs")
+
+    def test_link(self):
+        node = TextNode("sauce?", TextType.LINK, "www.images.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.value, "sauce?")
+        self.assertEqual(html_node.props_to_html(), f" href=\"{node.url}\"")
+
+    def test_image(self):
+        node = TextNode("pics or it didn't happen", TextType.IMAGE, "www.images.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props_to_html(), f" src=\"{node.url}\" alt=\"{node.text}\"")
