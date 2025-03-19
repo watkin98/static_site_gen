01a6676a58212e1b95f7e1d90826be9b414448e4
watkin98 <wvatkin1@gmail.com>
2025-03-19 15:54:55 -0700

    finished link extractor w/ successful tests

--- public/src/md_links_extractor.py
+++ public/src/md_links_extractor.py
@@ -5,9 +5,17 @@ def extract_markdown_images(text):
     Takes raw markdown text pertaining to an image as input and returns a list of tuples.
     Each tuple contains the alt text as its first index and the url of the image in the second index.
     '''
+    # The regex below is the implementation I came up with. Bootdev's is the following: 
+    # r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+
+    return re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
+
     
 def extract_markdown_links(text):
     '''
     Takes raw markdown text pertaining to a url as input and returns a list of tuples.
     Each tuple contains the anchor text as its first index and the url in the second index.
     '''
+    # The regex below is the implementation I came up with. Bootdev's is the following: 
+    # r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
--- public/tests/test_links_extractor.py
+++ public/tests/test_links_extractor.py
@@ -1,7 +1,9 @@
 import unittest
 from src.md_links_extractor import *
 
-class test_extractor(unittest.TestCase):
+class TestExtractor(unittest.TestCase):
+
+    # Below are the tests provided by Boot.dev
     def test_bootdev_image(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
         self.assertEqual(
@@ -11,7 +13,7 @@ class test_extractor(unittest.TestCase):
                             ], 
                             extract_markdown_images(text)
                         )
-
+    
     def test_bootdev_link(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         self.assertEqual(
@@ -23,7 +25,23 @@ class test_extractor(unittest.TestCase):
                         )
     
     def test_extract_markdown_images_bootdev(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images(self):
         matches = extract_markdown_images(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
