6f91e058a63762b789711b434414ead688a63bfd
watkin98 <wvatkin1@gmail.com>
2025-04-11 16:32:08 -0700

    wrote a bunch of tests

--- tests/test_html_handlers.py
+++ tests/test_html_handlers.py
@@ -56,14 +56,201 @@ This is another paragraph with _italic_ text and `code` here
             "<div><p><h1>This is a heading</h1></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
         )
 
+    def test_html_paragraph_with_multi_heading(self):
+        md = """
+### This is a heading
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><h3>This is a heading</h3></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
     def test_quotes(self):
-        pass
+        md = """
+> This is a quote
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><blockquote>This is a quote</blockquote></p></div>",
+        )
+
+    def test_quotes_extended(self):
+        md = """
+> This is a quote
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><blockquote>This is a quote</blockquote></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
 
     def test_unordered_lists(self):
-        pass
+        md = """
+- Item 1
+- Item 2
+- Item 3    
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul></p></div>",
+        )
+
+    def test_unordered_lists_extended(self):
+        md = """
+- Item 1
+- Item 2
+- Item 3
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
 
     def test_ordered_lists(self):
-        pass
+        md = """
+1. Item 1
+2. Item 2
+3. Item 3
 
+"""
 
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><ol><li>Item 1</li><li>Item 2</li><li>Item 3</li></ol></p></div>",
+        )
+
+    def test_ordered_lists_extended(self):
+        md = """
+1. Item 1
+2. Item 2
+3. Item 3
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><ol><li>Item 1</li><li>Item 2</li><li>Item 3</li></ol></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_links(self):
+        md = """
+This is a paragraph with a [link](https://www.google.com).
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p>This is a paragraph with a <a href=\"https://www.google.com\">link</a></p></div>",
+        )
+
+    def test_links_extended(self):
+        md = """
+This is a paragraph with a [link](https://www.google.com).
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p>This is a paragraph with a <a href=\"https://www.google.com\">link</a></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_images(self):
+        md = """
+![alt text for image](url/of/image.jpg)
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><img src=\"url/of/image.jpg\" alt=\"Description of image\"></p></div>",
+        )
+
+    def test_images_extended(self):
+        md = """
+![alt text for image](url/of/image.jpg)
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        #print(f"\nHTML: {html}")
+        self.assertEqual(
+            html,
+            "<div><p><img src=\"url/of/image.jpg\" alt=\"Description of image\"></p><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
         
