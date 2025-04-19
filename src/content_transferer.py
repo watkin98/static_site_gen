e8971109f4632fc1f8df2dd90565f45a72a6eb08
watkin98 <wvatkin1@gmail.com>
2025-04-18 17:00:33 -0700

    worked on file tranferer; stuck on handling working directories durring transfer

--- src/content_transferer.py
+++ src/content_transferer.py
@@ -2,13 +2,56 @@ import shutil
 import os
 
 def main():
-    static_to_public_transfer()
+    try:
+        static_to_public_transfer()
+    except Exception as e:
+        print(f"Did not work, clearing public directory. \nException: {e}")
+        shutil.rmtree("../public/")
+        os.mkdir("../public")
 
 def static_to_public_transfer():
     # Delete files and directories existing in public and create new public directory
-    print(f"1")
     shutil.rmtree("../public/")
     os.mkdir("../public")
-    print(f"2")
+
+    # Transfer the files
+    print()
+    file_tranferer("../static")
+
+def file_tranferer(fpath):
+    print(f"Current File Path: {fpath}")
+    # Get list of existing files and directories 
+    a_file = os.path.exists(fpath)
+    print(f"3 {a_file}")
+
+    if not a_file:
+        print(f"Item: {fpath}")
+        shutil.copy(f"{fpath}/{item}", "../public")
+    else:
+        print(f"{fpath}\nCwd: {os.getcwd()}")
+        entries = os.listdir(path=fpath)
+        print(f"{entries}")
+
+    for item in entries:
+        item_is_a_file_1 = file_determiner(item)
+        if item_is_a_file_1:
+            shutil.copy(f"{fpath}/{item}", "../public")
+        else:
+            file_tranferer(item)
+
+def file_determiner(fpath):
+    '''
+    Takes in a string that represents either a directory or a file. Return True if a file,
+    false if a Directory
+    '''
+    dot_count = 0
+    for char in fpath:
+        if char == '.':
+            dot_count += 1
+    
+    if dot_count == 1:
+        return True
+    
+    return False
 
 main()
