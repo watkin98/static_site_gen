7144d507ce7767837f90ac025c05f376e2aa71cb
watkin98 <wvatkin1@gmail.com>
2025-04-24 16:54:52 -0700

    FINALLY FINISHED CONTENT TRANSFERER LESSS GOOOOOO

--- src/content_transferer.py
+++ src/content_transferer.py
@@ -6,67 +6,60 @@ def main():
     try:
         static_to_public_transfer()
     except Exception as e:
-        print(f"Did not work, clearing public directory. \nException: {e}")
+        #print(f"Did not work, clearing public directory. \nException: {e}")
         traceback.print_exc()
-        #shutil.rmtree("../public/")
-        #os.mkdir("../public")
+        shutil.rmtree("../public/")
+        os.mkdir("../public")
 
 def static_to_public_transfer():
     # Delete files and directories existing in public and create new public directory
-    shutil.rmtree("../public/")
-    os.mkdir("../public")
+    shutil.rmtree("public/")
+    os.mkdir("public/")
 
     # Transfer the files
-    print()
-    file_transferer("../static")
+    #print()
+    file_transferer("static/")
 
-'''
-def file_tranferer(fpath):
-    print(f"Current File Path: {fpath}")
-    # Get list of existing files and directories 
-    a_file = os.path.exists(fpath)
-    print(f"3 {a_file}")
-
-    if not a_file:
-        #print(f"Item: {fpath}\nPath: {fpath}/{item}")
-        os.mkdir(f"../public/{fpath}")
-        shutil.copy(f"{fpath}", "../public")
-    else:
-        print(f"{fpath}\nCwd: {os.getcwd()}")
-        entries = os.listdir(path=fpath)
-        print(f"{entries}")
-
-    for item in entries:
-        item_is_a_file_1 = file_determiner(item)
-        if item_is_a_file_1:
-            shutil.copy(f"{fpath}/{item}", "../public")
-        else:
-            file_tranferer(item)
-'''
 def file_transferer(fpath):
     a_file = os.path.isfile(fpath)
-    print(f"Current fpath: {fpath}")
+    #print(f"Current fpath: {fpath}")
 
     if not a_file:
         directory_entires = os.listdir(path=fpath)
-        print(f'{directory_entires}')
+        #print(f'{directory_entires}')
 
     temp_dir = fpath
     
     for item in directory_entires:
         working_dir = f"{temp_dir}/{item}"
-        print(f"Dir: {working_dir}")
+        #print(f"Dir: {working_dir}")
         a_file = os.path.isfile(working_dir)
-        print(f"Item {item} is a file: {a_file}\nCwd: {working_dir}")
+        #print(f"Item {item} is a file: {a_file}\nCwd: {working_dir}")
         if a_file:
             file_copier(working_dir, item)
         else:
-            os.mkdir(f"../public/{item}")
-            print(f"Recursive Dir: {working_dir}\nfpath: {fpath}")
+            # Create new path for item in public if necessary
+            #os.mkdir(f"../public/{item}")
+            #print(f"Recursive Dir: {working_dir}\nfpath: {fpath}")
             file_transferer(working_dir)
 
 def file_copier(fpath, file):
-    print(f"Path: {fpath}\nFile: {file}")
-    shutil.copy(f"{fpath}", "../public")
+    sub_path = fpath[6:]
+    sub_path_new = ""
+
+    if '.' in sub_path:
+        #print("1")
+        sub_path_new = sub_path.replace(file, '')
+        #print(f"New Sub Dir: {sub_path_new}")
+
+    #print(f"Path: {fpath}\nSub Path: {sub_path}\nFile: {file}")
+
+    if sub_path_new == "":
+        shutil.copy(f"{fpath}", "public/")
+    elif os.path.exists(f"public/{sub_path_new}"):
+        shutil.copy(fpath, f"public/{sub_path_new}")
+    else:
+        os.mkdir(f"public/{sub_path_new}")
+        shutil.copy(f"{fpath}", f"public/{sub_path_new}")
 
 main()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import *
+from content_transferer import *
 
 def main():
     obj = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
     string = obj.__repr__
-    print(string)
+    #print(string)
+
+    static_to_public_transfer()
 
 main()
