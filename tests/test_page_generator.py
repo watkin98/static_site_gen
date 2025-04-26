b16ba130e1da03d968490c80a22efb0057f24866
watkin98 <wvatkin1@gmail.com>
2025-04-25 17:00:27 -0700

    progressed on page generator; left off on step 5_5

---
+++ tests/test_page_generator.py
@@ -0,0 +1,82 @@
+import unittest
+from src.page_generator import *
+
+class TestPageGenerator(unittest.TestCase):
+    def test_base(self):
+        markdown = """
+        # This is a h1 header
+
+        This is **bolded** paragraph
+
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+
+        - This is a list
+        - with items
+        """
+        test = extract_title(markdown)
+        self.assertEqual(test, "This is a h1 header")
+
+    def test_only_header(self):
+        markdown = """
+        # This is a h1 header
+        """
+        test = extract_title(markdown)
+        self.assertEqual(test, "This is a h1 header")
+
+    def test_misplaced_header(self):
+        markdown = """
+        This is **bolded** paragraph
+
+        # This is a h1 header
+
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+
+        - This is a list
+        - with items
+        """
+        with self.assertRaises(ValueError):
+            extract_title(markdown)
+
+    def test_h2(self):
+        markdown = """
+        ## This is a h1 header
+
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+
+        - This is a list
+        - with items
+        """
+        with self.assertRaises(ValueError):
+            extract_title(markdown)
+
+    def test_h6(self):
+        markdown = """
+        ###### This is a h1 header
+
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+
+        - This is a list
+        - with items
+        """
+        with self.assertRaises(ValueError):
+            extract_title(markdown)
+    
+    def test_paragraph(self):
+        markdown = """
+        This is a paragraph
+
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+
+        - This is a list
+        - with items
+        """
+        with self.assertRaises(ValueError):
+            extract_title(markdown)
+        
+    def test_generate_page(self):
+        generate_page("content/index.md", "template.html", "temp")
