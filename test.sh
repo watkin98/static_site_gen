87b37d42536ff0730bac2f334413cf6a6aacd7ed
watkin98 <wvatkin1@gmail.com>
2025-05-06 17:03:17 -0700

    focusing on passing bootdev submission tests, starting with errors in lists and quotes

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #python3 -m unittest discover -v -s tests
 #!/bin/bash
-PYTHONPATH=src python3 -m unittest discover tests
-#PYTHONPATH=src python3 -m unittest -v tests/test_text_converter.py
+#PYTHONPATH=src python3 -m unittest discover tests
+PYTHONPATH=src python3 -m unittest -v tests/test_block_handlers.py
+PYTHONPATH=src python3 -m unittest -v tests/test_html_handlers.py
 #python3 -m unittest -v tests/test_block_handlers.py
--- tests/test_block_handlers.py
+++ tests/test_block_handlers.py
@@ -134,3 +134,17 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+            block = "# heading"
+            self.assertEqual(block_to_block_type(block), BlockType.heading)
+            block = "```\ncode\n```"
+            self.assertEqual(block_to_block_type(block), BlockType.code)
+            block = "> quote\n> more quote"
+            self.assertEqual(block_to_block_type(block), BlockType.quote)
+            block = "- list\n- items"
+            self.assertEqual(block_to_block_type(block), BlockType.unordered_list)
+            block = "1. list\n2. items"
+            self.assertEqual(block_to_block_type(block), BlockType.ordered_list)
+            block = "paragraph"
+            self.assertEqual(block_to_block_type(block), BlockType.paragraph)
